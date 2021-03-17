e7e59baeeacd0309025bbb804cc7bda5599f9f83
Ludovic Ollagnier <ludovic@tectec.fr>
2021-03-17 13:57:08 +0100

    Playground demo - variables and types

---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+let name = "Ludovic"
+//name = "Toto"
+
+var age = 34
+age = 35
+age = 36
+age = 37
+
+var height = 1.83
+height = Double(age)
+
+var version: Float = 5.3
+version = Float(height)
+
+let a = 5.0
+let b = 2
+let c = a / Double(b)
+
+let result = Double("5,5")
