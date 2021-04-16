39d0f48de7d8854c7f07f999e2fcaed84f9c9250
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-04-16 18:12:30 +0200

    Inheritance + functions

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -45,3 +45,80 @@ let odo2 = Odometer()
 odo2.printMileage()
 //odo2.increment()
 //odo2.reset()
+
+class Vehicule {
+
+    let numberOfWheels: Int
+
+    init(numberOfWheels: Int) {
+        self.numberOfWheels = numberOfWheels
+    }
+
+    func makeNoise() {
+
+    }
+}
+
+class Bicycle: Vehicule {
+
+    var hasBasket: Bool
+
+    init(hasBasket: Bool) {
+        self.hasBasket = hasBasket
+        super.init(numberOfWheels: 2)
+    }
+
+    override func makeNoise() {
+        print("Dring dring")
+    }
+}
+
+// Functions
+
+// Simple
+func doSomething() {
+    print("I do something")
+}
+
+doSomething()
+doSomething()
+
+// avec paramètres
+func multiply(_ firstNumber: Int, by secondNumber: Int) {
+    let result = firstNumber * secondNumber
+    print(result)
+}
+
+multiply(5, by: 2)
+
+var array = ["Toto"]
+array.append("Titi")
+array.remove(at: 0)
+array.insert("Tata", at: 1)
+
+func sayHi(to name: String = "You") {
+    print("Hi, \(name)")
+}
+
+sayHi(to: "Ludovic")
+sayHi()
+
+print("A", "B", "C", separator: ":", terminator: "!")
+
+// Retours
+
+func add(_ firstNumber: Int, to secondNumber: Int) -> Int {
+    firstNumber + secondNumber
+}
+
+let resultAdd = add(5, to: 5)
+
+func substract(_ first: Int, to second: Int) -> (first: Int, second: Int, result: Int) {
+    return (first, second, second - first)
+}
+
+// Tuple Int dimension 3
+let resultSub = substract(5, to: 10)
+resultSub.result
+
+//func allSatisfy(predicate: (String) -> Bool)) -> Bool
