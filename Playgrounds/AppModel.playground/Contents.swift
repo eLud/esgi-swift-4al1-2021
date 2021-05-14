04489fbdb37715478d346ff6f3a9f16f83c288bc
Ludovic Ollagnier <ludovic@tectec.fr>
2021-05-14 12:31:09 +0200

    Correction classe + optionals

--- Playgrounds/AppModel.playground/Contents.swift
+++ Playgrounds/AppModel.playground/Contents.swift
@@ -2,12 +2,12 @@ import UIKit
 
 // Nom des types commencent par des majuscules
 
-struct Ingredient {
+struct Ingredient: Equatable {
     let name: String
     let isVeggie: Bool
 }
 
-struct Meal {
+struct Meal: Equatable {
 
     // Nom des propriétés commencent par des minuscules
     let name: String
@@ -41,3 +41,62 @@ let ingredients = [Ingredient(name: "Riz", isVeggie: true), Ingredient(name: "To
 let myMeal = Meal(name: "Riz à la tomate", ingredients: ingredients, price: 8, pitch: "Un super plat", preciseDescription: "")
 
 myMeal.isVeggie
+
+
+class Restaurant {
+
+    let name: String
+    let address: String?
+    var meals: [Meal]
+    let pitch: String
+
+    var mediumMealPrice: Double {
+
+        guard !meals.isEmpty else { return 0.0 } //Équivalent au if juste en dessous
+        if meals.isEmpty { return 0.0 }
+//        guard meals.count < Int.max else { return 0.0 }
+
+        let totalPrice = meals.reduce(0.0) { (result, meal) -> Double in
+            result + meal.price
+        }
+        return totalPrice / Double(meals.count)
+    }
+
+    init(name: String, address: String?, pitch: String, meals: [Meal]) {
+        self.name = name
+        self.address = address
+        self.pitch = pitch
+        self.meals = meals
+    }
+
+    func add(_ meal: Meal) {
+        guard !meals.contains(meal) else { return }
+     }
+
+    func remove(_ meal: Meal) {
+
+        if let firstIndex = meals.firstIndex(of: meal) {
+            meals.remove(at: firstIndex)
+        }
+
+        guard let firstIndex2 = meals.firstIndex(of: meal) else { return }
+        meals.remove(at: firstIndex2)
+    }
+}
+
+let resto = Restaurant(name: "", address: nil, pitch: "", meals: [])
+resto.add(myMeal)
+resto.remove(myMeal)
+
+let dico: [String: Int] = ["age": 12]
+let result: Optional<Int> = dico[""]
+if let result = dico["age"] {
+    print(result)
+}
+
+
+
+
+
+
+
